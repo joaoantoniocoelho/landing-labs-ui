01aa5cf14f1654f057ed7e65ba72217f93ab7194
João Antonio Coelho <joaoantonioscoelho@gmail.com>
2024-09-20 01:58:04 -0300

    update theme

--- theme/index.js
+++ theme/index.js
@@ -3,20 +3,15 @@ import { extendTheme } from '@chakra-ui/react';
 const colors = {
     brand: {
         primary: '#2ecc71',       
-        secondary: '#ffffff',    
+        secondary: '#fefefe',    
         background: '#ecf0f1',   
     },
 
     text: {
         primary: '#333333',  
-        secondary: '#ffffff',     
+        secondary: '#fefefe',     
     },
-
-    status: {
-        error: '#e53e3e',         
-        success: '#38a169',     
-    },
-
+    
     interaction: {
         greenHover: '#27ae60',    
     },
