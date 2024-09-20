995603517cd60022d94003a963950128a78ac2c7
João Antonio Coelho <joaoantonioscoelho@gmail.com>
2024-09-20 19:06:51 -0300

    update theme

--- theme/theme.js
+++ theme/theme.js
@@ -8,13 +8,17 @@ const theme = extendTheme({
     },
     colors: {
         brand: {
-            primary: '#2ecc71',
-            secondary: '#fefefe',
-            background: '#ecf0f1',
-            text: '#333333',
+            primary: '#2ecc71',       
+            secondary: '#fefefe',    
+            background: '#ecf0f1',   
         },
+        text: {
+            primary: '#333333',  
+            secondary: '#fefefe',     
+        },
+            
         interaction: {
-            greenHover: '#27ae60',
+            greenHover: '#27ae60',    
         },
     },
 });
