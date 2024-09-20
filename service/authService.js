5aa290b4cfa2b415218f2660af3dd525afc02ca2
João Antonio Coelho <joaoantonioscoelho@gmail.com>
2024-09-20 02:08:41 -0300

    fix api login endpoint call

--- service/authService.js
+++ service/authService.js
@@ -1,7 +1,7 @@
 import api from './api';
 export const login = async (email, password) => {
     try {
-        const response = await api.post('/users/login', {
+        const response = await api.post('/auth/login', {
             email,
             password,
         });
