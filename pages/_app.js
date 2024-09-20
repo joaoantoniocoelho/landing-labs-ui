0182554dcf3243ddf28e80617bb38cdfd2c1db15
João Antonio Coelho <joaoantonioscoelho@gmail.com>
2024-09-20 19:15:16 -0300

    add page header

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,16 @@
 import { ChakraProvider } from "@chakra-ui/react";
-import customTheme from '../theme/theme'
+import customTheme from '../theme/theme';
+import Head from 'next/head';
 
 function App({ Component, pageProps }) {
     return (
         <ChakraProvider theme={customTheme}>
+            <Head>
+                <title>Landing Labs</title>
+            </Head>
             <Component {...pageProps} />
         </ChakraProvider>
-    )
+    );
 }
 
 export default App;
