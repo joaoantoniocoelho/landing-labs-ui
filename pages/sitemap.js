b202b93d2cf048a8e418b154e9467c89cb4debe5
joão coelho <joaoantonioscoelho@gmail.com>
2024-09-26 10:08:31 -0300

    fix baseUrl on sitemap.js

--- pages/sitemap.js
+++ pages/sitemap.js
@@ -1,7 +1,7 @@
 export async function getServerSideProps({ res }) {
-    const baseUrl = 'https://www.pageexpress.io'; 
+    const baseUrl = 'https://pageexpress.io'; 
 
-    const staticPages = ['/', '/login', '/register'];
+    const staticPages = ['/'];
   
     const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
       <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
