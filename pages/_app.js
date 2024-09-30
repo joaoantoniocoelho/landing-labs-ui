927a22c18972f483509c8419f28c521fa3f83f63
João Coelho <joaoantonioscoelho@gmail.com>
2024-09-30 01:25:43 -0300

    create coming soon page

--- pages/_app.js
+++ pages/_app.js
@@ -2,8 +2,18 @@ import { ChakraProvider } from '@chakra-ui/react';
 import customTheme from '../theme/theme';
 import Head from 'next/head';
 import '../pages/globals.css';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 function App({ Component, pageProps }) {
+    const router = useRouter();
+
+    useEffect(() => {
+        if (router.pathname !== '/' && router.pathname !== '/coming-soon') {
+            router.replace('/coming-soon');
+        }
+    }, [router.pathname]);
+
     return (
         <ChakraProvider theme={customTheme}>
             <Head>
