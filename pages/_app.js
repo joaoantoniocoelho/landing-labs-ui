9282da5f5cc0425b698fac9d329cb87f60b5133c
João Coelho <joaoantonioscoelho@gmail.com>
2024-09-30 01:31:23 -0300

    fix merge conflicts

--- pages/_app.js
+++ pages/_app.js
@@ -20,7 +20,7 @@ function App({ Component, pageProps }) {
                 <title>Page Express</title>
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
             </Head>
-            <ProtectedComponent {...pageProps} />
+            <Component {...pageProps} />
         </ChakraProvider>
     );
 }
